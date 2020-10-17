3b7d710994706d050d0ffa515df94ed171d7bdb6
Cole Michaels <cmichaelsd@gmail.com>
2020-10-17 10:55:28 -0700

    converting to a better functional style over oop classes.

---
+++ arabic_to_sino_korean.py
@@ -0,0 +1,43 @@
+def remove_decimal_place(value, decimal_place):
+    decimal_place *= 10
+    difference = value - (value % decimal_place)
+    result = difference / decimal_place
+    return int(result)
+
+def get_decimal_place(value, decimal_place):
+    decimal_place *= 10
+    return value % decimal_place
+
+
+def arabic_to_sino_korean(value):
+    if value > 999999999999:
+        print("Error: Sino-Korean number must be less than 999,999,999,999.")
+        return
+
+    result = ""
+    decimal_index = 0
+    sino_numerals = ["공", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"]
+    sino_ten_thousands_place = { 0: "", 4: "만", 8: "억" }
+    sino_decimal_place = { 1: "십", 2: "백", 3: "천" }
+
+    if value == 0: return sino_numerals[value]
+
+    while True:
+        current_decimal = get_decimal_place(value, 1)
+        value = remove_decimal_place(value, 1)
+
+        if decimal_index % 4 == 0:
+            result += sino_ten_thousands_place[decimal_index]
+            
+        elif decimal_index > 0 and current_decimal > 0:
+            result += sino_decimal_place[decimal_index % 4]
+
+        if current_decimal > 1 or (current_decimal > 0 and decimal_index == 0):   
+            result += sino_numerals[current_decimal]
+
+        decimal_index += 1
+
+        if value == 0:
+            break
+    
+    return result[::-1]
--- tests.py
+++ tests.py
@@ -1,34 +1,25 @@
-from arabic_to_korean import ArabicToKorean
-from typing import Any
+from arabic_to_sino_korean import arabic_to_sino_korean
 
-class Expect:
-    def __init__(self, result) -> None:
-        self.__result = result
-    
-    @property
-    def result(self) -> str:
-        return self.__result
-    
-    def to_equal(self, expect) -> None:
-        if expect == self.result:
-            print("Passed")
-        else:
-            print(f"Failed: expected {expect}, result {self.result}.")
+def to_equal(result, expect):
+    if expect == result:
+        print("Passed")
+    else:
+        print(f"Failed: expected {expect}, result {result}.")
     
 
 
 ### Sino-Korean Tests ###
-Expect(ArabicToKorean("3524761")).to_equal("삼백오십이만사천칠백육십일")
-Expect(ArabicToKorean("0000")).to_equal("공")
-Expect(ArabicToKorean("1212")).to_equal("천이백십이")
-Expect(ArabicToKorean("1000")).to_equal("천")
-Expect(ArabicToKorean("1")).to_equal("일")
-Expect(ArabicToKorean("10")).to_equal("십")
-Expect(ArabicToKorean("50")).to_equal("오십")
-Expect(ArabicToKorean("999999999999")).to_equal("구천구백구십구억구천구백구십구만구천구백구십구")
+to_equal(arabic_to_sino_korean(3524761), "삼백오십이만사천칠백육십일")
+to_equal(arabic_to_sino_korean(0), "공")
+to_equal(arabic_to_sino_korean(1212), "천이백십이")
+to_equal(arabic_to_sino_korean(1000), "천")
+to_equal(arabic_to_sino_korean(1), "일")
+to_equal(arabic_to_sino_korean(10), "십")
+to_equal(arabic_to_sino_korean(50), "오십")
+to_equal(arabic_to_sino_korean(999999999999), "구천구백구십구억구천구백구십구만구천구백구십구")
 
 ### Native Korean Tests ###
-Expect(ArabicToKorean("1", False)).to_equal("하나")
-Expect(ArabicToKorean("10", False)).to_equal("열")
-Expect(ArabicToKorean("55", False)).to_equal("쉰다섯")
-Expect(ArabicToKorean("0", False)).to_equal("$") # Fails correctly, returns $
+# Expect(ArabicToKorean("1", False)).to_equal("하나")
+# Expect(ArabicToKorean("10", False)).to_equal("열")
+# Expect(ArabicToKorean("55", False)).to_equal("쉰다섯")
+# Expect(ArabicToKorean("0", False)).to_equal("$") # Fails correctly, returns $
